010f5305cce92151fb8c71b3ad152dcf6697510d
srshchkv <125016193+srshchkv@users.noreply.github.com>
2024-06-09 23:36:35 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,15 +1,25 @@
 import socket
 import pickle
+import rsa
 
 HOST = '127.0.0.1'
 PORT = 8080
 
-sock = socket.socket()
-sock.bind((HOST, PORT))
-sock.listen(1)
-conn, addr = sock.accept()
+public_key, private_key = rsa.newkeys(1024)
 
-msg = conn.recv(1024)
-print(pickle.loads(msg))
+with socket.socket() as sock:
+    sock.bind((HOST, PORT))
+    sock.listen(1)
+    conn, addr = sock.accept()
+    msg = conn.recv(1024)
+    client_public_key = pickle.loads(msg)
 
-conn.close()
+    conn.send(pickle.dumps(public_key))
+
+    encrypted_message = conn.recv(1024)
+    message = rsa.decrypt(encrypted_message, private_key)
+    print("Получено сообщение:", message.decode())
+
+    response = "Привет, клиент!".encode()
+    encrypted_response = rsa.encrypt(response, client_public_key)
+    conn.send(encrypted_response)
